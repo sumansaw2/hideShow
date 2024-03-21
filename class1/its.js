37d6a364b1b4502188079a9ff70de02262688133
sumansaw2 <164318366+sumansaw2@users.noreply.github.com>
2024-03-21 13:18:17 +0530

    Add files via upload

---
+++ class1/its.js
@@ -0,0 +1,40 @@
+// $(document).ready(function(){});
+
+// $(function(){
+//     $('.btn1').click(function(){
+//         $('p').hide()
+//     });
+//     $('#btn2').click(function(){
+//         $('p').show()
+//     });
+//     $('#btn3').click(function(){
+//         $('p').toggle()
+//     });
+
+// });
+// $(function(){
+//     $('.btn1').click(function(){
+//         $('img').hide()
+//     });
+//     $('#btn2').click(function(){
+//         $('img').show()
+
+//     });
+//     $('#btn3').click(function(){
+//         $('img').toggle();
+//     })
+
+// });
+   
+$(document).ready(function(){
+    $('.btn1').click(function(){
+        $('img').hide()
+    })
+    $('#btn2').click(function(){
+        $('img').show()
+    })
+    $('#btn3').click(function(){
+        $('img').toggle()
+    })
+    
+})
